f1cb62eaaf579376b3f4bfd5bf14d427b0bdffff
LorDClockaN <lordclockan@gmail.com>
2016-10-09 22:10:32 +0200

    ELIXIR: ELIXIR Logo on Status Bar (2/2)
    Change-Id: Ia6309c8039cc3484b0220527d741cf26c6c4fa9e

--- src/com/elixir/settings/fragments/StatusBarSettings.java
+++ src/com/elixir/settings/fragments/StatusBarSettings.java
@@ -31,17 +31,63 @@ import java.util.ArrayList;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+        private static final String KEY_ELIXIR_LOGO_COLOR = "status_bar_elixir_logo_color";
+        private static final String KEY_ELIXIR_LOGO_STYLE = "status_bar_elixir_logo_style";
+
+        private ColorPickerPreference mElixirLogoColor;
+        private ListPreference mElixirLogoStyle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.elixir_settings_statusbar);
 
+            PreferenceScreen prefSet = getPreferenceScreen();
+            final ContentResolver resolver = getActivity().getContentResolver();
+            Context context = getActivity();
+
+            mElixirLogoStyle = (ListPreference) findPreference(KEY_ELIXIR_LOGO_STYLE);
+            int elixirLogoStyle = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_ELIXIR_LOGO_STYLE, 0,
+                    UserHandle.USER_CURRENT);
+            mElixirLogoStyle.setValue(String.valueOf(elixirLogoStyle));
+            mElixirLogoStyle.setSummary(mElixirLogoStyle.getEntry());
+            mElixirLogoStyle.setOnPreferenceChangeListener(this);
+
+            // Aicp logo color
+            mElixirLogoColor =
+                (ColorPickerPreference) prefSet.findPreference(KEY_ELIXIR_LOGO_COLOR);
+            mElixirLogoColor.setOnPreferenceChangeListener(this);
+            int intColor = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_ELIXIR_LOGO_COLOR, 0xffffffff);
+            String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mElixirLogoColor.setSummary(hexColor);
+            mElixirLogoColor.setNewPreviewColor(intColor);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mElixirLogoColor) {
+                String hex = ColorPickerPreference.convertToARGB(
+                        Integer.valueOf(String.valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(resolver,
+                        Settings.System.STATUS_BAR_ELIXIR_LOGO_COLOR, intHex);
+                return true;
+            } else if (preference == mElixirLogoStyle) {
+                int elixirLogoStyle = Integer.valueOf((String) newValue);
+                int index = mElixirLogoStyle.findIndexOfValue((String) newValue);
+                Settings.System.putIntForUser(
+                        resolver, Settings.System.STATUS_BAR_ELIXIR_LOGO_STYLE, elixirLogoStyle,
+                        UserHandle.USER_CURRENT);
+                mElixirLogoStyle.setSummary(
+                        mElixirLogoStyle.getEntries()[index]);
+                return true;
+            }
         return false;
     }
 
